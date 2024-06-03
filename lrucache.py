59f52ab6bd7c819173f93d7ac29b78a8a4af1803
kwura <kevin.wurangian@gmail.com>
2024-06-03 13:10:08 +0800

    Added concise version

--- lrucache.py
+++ lrucache.py
@@ -1,77 +1,41 @@
 class Node:
-  def __init__(self, key, val, prev=None, next= None):
-    self.key = key
-    self.val = val
-    self.prev = prev
-    self.next = next
+    def __init__(self, key=0, val=0):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
 
 class LRUCache:
     def __init__(self, capacity: int):
-      self.capacity = capacity
-      self.hash = dict()
-      self.lru = None
-      self.mru = None
+        self.cap = capacity
+        self.cache = dict()
+        self.left, self.right = Node(), Node()
+        self.left.next, self.right.prev = self.right, self.left
+    
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    
+    def insert(self, node):
+        self.right.prev.next = node
+        node.prev = self.right.prev
+        self.right.prev = node
+        node.next = self.right
 
     def get(self, key: int) -> int:
-      if(key not in self.hash):
+        if(key in self.cache):
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
         return -1
 
-      node = self.hash[key] 
-      if(node != self.lru and node != self.mru):
-        node.next.prev = node.prev
-        node.prev.next = node.next
-
-      if(node == self.lru and node.prev):
-        self.lru = node.prev
-        self.lru.next = None
-      
-      if(node != self.mru):
-        self.mru.prev = node
-        node.next = self.mru
-        self.mru = node
-        node.prev = None
-      
-      return node.val
-
     def put(self, key: int, value: int) -> None:
-      if(key in self.hash):
-        node = self.hash[key]
-        node.val = value
+        if(key in self.cache):
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
 
-        if(node != self.lru and node != self.mru):
-          node.next.prev = node.prev
-          node.prev.next = node.next
-        if(self.lru == node and node.prev):
-          self.lru = node.prev
-          self.lru.next = None
-        if(self.mru != node):
-          self.mru.prev = node
-          node.next = self.mru
-          self.mru = node
-          node.prev = None
-      else:
-        if(len(self.hash) < self.capacity):
-          node = Node(key=key, val=value)
-          self.hash[key] = node
-          if(not self.lru):
-            self.lru = node
-          if(not self.mru):
-            self.mru = node
-          else:
-            self.mru.prev = node
-            node.next = self.mru
-            self.mru = node
-        elif(len(self.hash) == self.capacity):
-          node = Node(key=key, val=value)
-          del self.hash[self.lru.key]
-          self.hash[key] = node
-  
-          self.mru.prev = node
-          node.next = self.mru
-          self.mru = node
-  
-          if(self.lru.prev):
-            self.lru = self.lru.prev
-          else:
-            self.lru = node
-          self.lru.next = None
+        if(len(self.cache) > self.cap):
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
